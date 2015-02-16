9a2960840a7bfcfab161f0c3b1b584ab4be5f514
Stephen Sloat <stephenlsloat@gmail.com>
2015-02-16 11:18:35 -0600

    add 2015 budget info

--- python/finances/budget.py
+++ python/finances/budget.py
@@ -30,10 +30,10 @@ class Budget(Base):
 class BudgetEntry(Base):
     __tablename__ = 'budget_entries'
 
-    id     = Column(Integer, primary_key=True)
-    name   = Column(String)
-    monthy = Column(Float)
-    yearly = Column(Float)
+    id         = Column(Integer, primary_key=True)
+    name       = Column(String)
+    monthy     = Column(Float)
+    yearly     = Column(Float)
 
     budget_id   = Column(Integer, ForeignKey('budgets.id'))
     category_id = Column(Integer, ForeignKey('categories.id'))
@@ -55,3 +55,47 @@ class BudgetEntry(Base):
     def amount(self):
         return 12*self.monthly + self.yearly
 
+if __name__ == 'main':
+    b = Budget('2015 Single', 2015, 'Single')
+
+    top        = Category('top', None, 0)
+    income     = Category('income', top)
+    taxable    = Category('taxable income', income)
+    nontaxable = Category('nontaxable income', income)
+
+    bofa = BudgetEntry('Bofa', b, taxable, 136000/12.0, 25000)
+    BudgetEntry('Bofa 401k match', b, nontaxable, 0.05*18000/12, 0.03*bofa.amount())
+
+    BudgetEntry('Rent', b, taxable, 12*(450+450+400))
+
+    pretax = Category('pretax', top)
+    BudgetEntry('401k', b, pretax, 18000/12.0)
+    BudgetEntry('Health Insurance', b, pretax, 2.0*48.37)
+    BudgetEntry('Dental', b, pretax, 2*9.67)
+    BudgetEntry('Vision', b, pretax, 2*4.67)
+    BudgetEntry('Metra', b, pretax, 130)
+    BudgetEntry('HSA', b, pretax, 100)
+
+    giving = Category('giving', top)
+    BudgetEntry('John Stott', b, giving, 250)
+    BudgetEntry('Compassion', b, giving, 286)
+    BudgetEntry('GFA',        b, giving, 210)
+    BudgetEntry('Stars',      b, giving, 200)
+    BudgetEntry('Keane',      b, giving, 75)
+    BudgetEntry('Keith',      b, giving, 125)
+    BudgetEntry('Stanley',    b, giving, 100)
+    BudgetEntry('Rez',        b, giving, 2600, 5700)
+
+    house = Category('house', top)
+    BudgetEntry('House Insurance', b, house, 800/12.0)
+    BudgetEntry('Principal', b, house, 5868/12.0)
+    BudgetEntry('Interest', b, house, 10135/12.0)
+    BudgetEntry('Property Tax', b, house, 0, 10185)
+
+    utilities = Category('utilities', top)
+    BudgetEntry('Nicor', b, utilities, 125)
+    BudgetEntry('Water', b, utilities, 125)
+    BudgetEntry('ComEd', b, utilities, 125)
+    BudgetEntry('TV/Internet', b, utilities, 125)
+
+    
