c7faf4e0cfb31c014dbd9f0e33594fb8ab9c7739
Stephen Sloat <stephenlsloat@gmail.com>
2015-03-02 19:54:16 -0600

    still starting out

--- perl/401k.pl
+++ perl/401k.pl
@@ -7,20 +7,16 @@ use Data::Dumper;
 chomp(my @lines = read_file('files/401k 2014.htm'));
 
 my $table = q{};
-my $rec=0;
-for my $l (@lines) {
-    if ($l =~ m#.*<table ([^/]*?)>#) {
-        my $table = $1;
-        my %kvs = map {split /=/} grep {s/"//g} split /\s+/, $table;
+for my $line (@lines) {
+    if ($line =~ m#.*<table ([^/]*?)>#) {
+        my %kvs = map {split /=/} grep {s/"//g} split /\s+/, $1;
         if (($kvs{'id'} // q{}) eq 'sortable') {
-            $rec=1;
+            $table = $1;
         }
     }
-    elsif ($l =~ m#.*</table>.*#) {
-        $rec=0;
-    }
-    elsif ($rec) {
-        ($l) = ($l =~ m#\s*(.*)\s*#);
+    elsif ($table) {
+        last if $line =~ m#.*</table>.*#;
+        my ($l) = ($line =~ m#\s*(.*)\s*#);
         $table .= " $l";
     }
 }
@@ -31,7 +27,7 @@ for my $row ($table =~ m#(<tr[ >].*?</tr>)#g) {
     s/<.*?>//g for @tds;
     next if !@tds;
     if ($tds[0] =~ m#^(\d\d/\d\d/\d\d\d\d)#) {
-        push @entries, [$1, @tds[1, 2]];
+        push @entries, [$1, @tds[1..4]];
     }
     elsif ($tds[1] ne 'Sources') {
         push @{ $entries[-1] }, [@tds[1..$#tds]];
@@ -42,16 +38,30 @@ for my $row ($table =~ m#(<tr[ >].*?</tr>)#g) {
 my %types;
 for (@entries) {
     my @entry = @$_;
-    next if $entry[2] ne 'CONTRIBUTION';
+    if ($entry[2] eq 'CONTRIBUTION') {
 
-    for my $item (@entry[3..$#entry]) {
-        next if !$item->[0];
-        my $k = $item->[0] eq '07 - PRE TAX CONTRIBUTIONS' ? 'me' : 'BofA';
+        for my $item (@entry[5..$#entry]) {
+            next if !$item->[0];
+            my $k = 
+                $item->[0] eq '07 - PRE TAX CONTRIBUTIONS'  ? 'me' 
+              : $item->[0] eq '14 - SAFE HARBOR MATCH'      ? 'BofA Match' 
+              : $item->[0] eq '20 - ANNUAL COMPANY CONTRIB' ? 'Pension' 
+              : undef;
 
-        my $amt = substr $item->[1], 1;
-        $amt =~ s/,//g;
-        print "$entry[0], $entry[1], $k, $amt\n";
-        $types{$k} += $amt;
+            if (!$k) {
+                print "Unknown:  $item->[0]\n";
+                next;
+            }
+
+            $item->[1] =~ s/[\$,]//g;
+            print "$entry[0], $entry[1], $k, $item->[1], $item->[2]\n";
+            $types{$k} += $item->[1];
+        }
+    }
+    elsif ($entry[2] eq 'Dividends' || $entry[2] eq 'RECORDKEEPING FEE') {
+        $entry[3] =~ s/[\$,]//g;
+        print "$entry[0], $entry[1], $entry[2], $entry[3], $entry[4]\n";
+        $types{$entry[2]} += $entry[3];
     }
 }
 
